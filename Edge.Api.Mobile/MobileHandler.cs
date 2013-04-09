27fd808d673a6034b6c401bb4fec94bc100c3ca3
sbarchen <sbarchen@5fdafd2f-8e2f-4cb0-84f8-1ba4a0fafccb>
2013-04-09 06:13:15 +0000

    Edge.API for Mobile

---
+++ Edge.Api.Mobile/MobileHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Edge.Api.Handlers.Template;
+using Edge.Objects;
+
+
+
+namespace Edge.Api.Mobile.Handlers
+{
+	public class MobileHandler  :TemplateHandler
+	{
+		[UriMapping(Method = "GET", Template = "performance/accounts/{accountID}/{period}")]
+		public List<Performance7Days> GetPerformanceLast7DaysByAccount(string accountID,string period)
+		{
+			List<Performance7Days> performance = new List<Performance7Days>();
+			performance = Performance.GetPerformanceByAccountIDAndPeriod(int.Parse(accountID), int.Parse(period));
+
+			return performance;
+		}
+
+		public override bool ShouldValidateSession
+		{
+			get { return true; }
+		}
+	}
+}
