40608e554c681cdd8377e3d635d43b8c5fe9ab45
shirat <shirat@5fdafd2f-8e2f-4cb0-84f8-1ba4a0fafccb>
2013-05-30 07:22:58 +0000

    Add logger to Mobile API ROAS split month and year Add settings response

--- trunk/Edge.WebApi.Mobile/Controllers/LoginController.cs
+++ trunk/Edge.WebApi.Mobile/Controllers/LoginController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Web.Http;
 using Edge.Api.Handlers;
+using Edge.Core.Utilities;
 using Edge.Objects;
 
 namespace Edge.WebApi.Mobile.Controllers
@@ -27,10 +28,12 @@ namespace Edge.WebApi.Mobile.Controllers
 			try
 			{
 				var response = handler.LogIn(sessionData);
+				Log.Write("Mobile API", String.Format("User {0} logged in to Mobile Application", response.UserID), LogMessageType.Debug);
 				return new ExtendedSessionResponseData {UserID = response.UserID, Session = response.Session};
 			}
 			catch (Exception ex)
 			{
+				Log.Write("Mobile API", String.Format("Login failed for user email '{0}', ex: {1}", email, ex.Message), LogMessageType.Error);
 				return new ExtendedSessionResponseData 
 				{
 					HasError = true, 
--- trunk/Edge.WebApi.Mobile/Controllers/SettingsController.cs
+++ trunk/Edge.WebApi.Mobile/Controllers/SettingsController.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Web.Http;
 using Edge.Api.Mobile.Settings;
 using Edge.Core.Configuration;
+using Edge.Core.Utilities;
 using Edge.Objects;
 
 namespace Edge.WebApi.Mobile.Controllers
@@ -9,30 +11,72 @@ namespace Edge.WebApi.Mobile.Controllers
     public class SettingsController : BaseController
     {
 		[ActionName("Themes")]
-		public List<SegmentInfo> GetThemes(string sessionId, int accountID)
+		public SettingsResponse GetThemes(string sessionId, int accountId)
 		{
-			ValidateSession(sessionId);
+			try
+			{
+				ValidateSession(sessionId);
 
-			var manager = GetManager();
-			return manager.GetSegmentInfo(accountID, SegmentType.Theme);
+				Log.Write("Mobile API", String.Format("Themes list request for account={0}", accountId), LogMessageType.Debug);
+				var manager = GetManager();
+				return manager.GetSegmentInfo(accountId, SegmentType.Theme);
+			}
+			catch (Exception ex)
+			{
+				Log.Write("Mobile API", String.Format("Get themes failed for account {0}, ex: {1}", accountId,  ex.Message), LogMessageType.Error);
+				return new SettingsResponse
+					{
+						HasError = true,
+						ErrorMsg = ex.Message,
+						DisplayError = (ex is MobileApiException) ? (ex as MobileApiException).DisplayMessage : String.Format("Failed to load Themes for account {0}", accountId)
+					};
+			}
 		}
 
 		[ActionName("Countries")]
-		public List<SegmentInfo> GetCountries(string sessionId, int accountID)
+		public SettingsResponse GetCountries(string sessionId, int accountId)
 		{
-			ValidateSession(sessionId);
+			try
+			{
+				ValidateSession(sessionId);
 
-			var manager = GetManager(); 
-			return manager.GetSegmentInfo(accountID, SegmentType.Country);
+				Log.Write("Mobile API", String.Format("Country list request for account={0}", accountId), LogMessageType.Debug);
+				var manager = GetManager(); 
+				return manager.GetSegmentInfo(accountId, SegmentType.Country);
+			}
+			catch (Exception ex)
+			{
+				Log.Write("Mobile API", String.Format("Get countries failed for account {0}, ex: {1}", accountId,  ex.Message), LogMessageType.Error);
+				return new SettingsResponse
+					{
+						HasError = true,
+						ErrorMsg = ex.Message,
+						DisplayError = (ex is MobileApiException) ? (ex as MobileApiException).DisplayMessage : String.Format("Failed to load Countries for account {0}", accountId)
+					};
+			}
 		}
 
 		[ActionName("Accounts")]
-		public List<SegmentInfo> GetAccounts(string sessionId)
+		public SettingsResponse GetAccounts(string sessionId)
 		{
-			var userId = ValidateSession(sessionId);
+			try
+			{
+				var userId = ValidateSession(sessionId);
 
-			var manager = GetManager();
-			return manager.GetAccounts(userId);
+				Log.Write("Mobile API", String.Format("Account list request for user={0}", userId), LogMessageType.Debug);
+				var manager = GetManager();
+				return manager.GetAccounts(userId);
+			}
+			catch (Exception ex)
+			{
+				Log.Write("Mobile API", String.Format("Get accounts failed, ex: {0}", ex.Message), LogMessageType.Error);
+				return new SettingsResponse
+					{
+						HasError = true,
+						ErrorMsg = ex.Message,
+						DisplayError = (ex is MobileApiException) ? (ex as MobileApiException).DisplayMessage : "Failed to load Accounts"
+					};
+			}
 		}
 
 		#region Private Methods
